5354e554912c85518f1cf2b0505712f0968bccb3
kaiyaok2 <kaiyaok2@illinois.edu>
2024-06-29 04:18:23 +0800

    Revert "c4"
    This reverts commit de5f7044a7fa438d4439e661611bc6ee69c3f0ba.

--- src/test/java/org/apache/commons/pool2/PoolTest.java
+++ src/test/java/org/apache/commons/pool2/PoolTest.java
@@ -65,4 +65,28 @@ public class PoolTest {
         }
     }
 
+    @Test
+    public void testPool() throws Exception {
+        final GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setTestWhileIdle(true /* testWhileIdle */);
+        final PooledFooFactory pooledFooFactory = new PooledFooFactory();
+        try (GenericObjectPool<Foo> pool = new GenericObjectPool<>(pooledFooFactory, poolConfig)) {
+            pool.setTimeBetweenEvictionRunsMillis(EVICTION_PERIOD_IN_MILLIS);
+            pool.addObject();
+            try {
+                Thread.sleep(EVICTION_PERIOD_IN_MILLIS);
+            } catch (final InterruptedException e) {
+                Thread.interrupted();
+            }
+        }
+        final Thread[] threads = new Thread[Thread.activeCount()];
+        Thread.enumerate(threads);
+        for (final Thread thread : threads) {
+            if (thread == null) {
+                continue;
+            }
+            final String name = thread.getName();
+            assertFalse( name.contains(COMMONS_POOL_EVICTIONS_TIMER_THREAD_NAME),name);
+        }
+    }
 }
